b51e36c89936b4091e6b851a5709b881e1b61e25
[Projet] <[r.marcelli31@gmail.com]>
2024-05-27 00:40:32 +0200

    add all page fruit etc + page produit

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const selectionDiv = document.querySelector('.selection');
+const checkboxesDiv = document.querySelector('.checkboxes');
+
+selectionDiv.addEventListener('click', function () {
+    checkboxesDiv.style.display = checkboxesDiv.style.display === 'block' ? 'none' : 'block';
+});
+
+const regionDiv = document.querySelector('.region');
+const checkboxes_regionDiv = document.querySelector('.checkboxes_region');
+
+regionDiv.addEventListener('click', function () {
+    checkboxes_regionDiv.style.display = checkboxes_regionDiv.style.display === 'block' ? 'none' : 'block';
+});
+
+const producteurDiv = document.querySelector('.producteur');
+const checkboxes_producteurDiv = document.querySelector('.checkboxes_producteur');
+
+producteurDiv.addEventListener('click', function () {
+    checkboxes_producteurDiv.style.display = checkboxes_producteurDiv.style.display === 'block' ? 'none' : 'block';
+});
+
+const labelDiv = document.querySelector('.label');
+const checkboxes_labelDiv = document.querySelector('.checkboxes_label');
+
+labelDiv.addEventListener('click', function () {
+    checkboxes_labelDiv.style.display = checkboxes_labelDiv.style.display === 'block' ? 'none' : 'block';
+});
+
+const noteDiv = document.querySelector('.note');
+const checkboxes_noteDiv = document.querySelector('.checkboxes_note');
+
+noteDiv.addEventListener('click', function () {
+    checkboxes_noteDiv.style.display = checkboxes_noteDiv.style.display === 'block' ? 'none' : 'block';
+});
+
 var sidenav = document.getElementById("mySidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
@@ -20,3 +55,4 @@ document.getElementById('trier-toggle').addEventListener('click', function() {
       element.classList.toggle('visible');
   });
 });
+
